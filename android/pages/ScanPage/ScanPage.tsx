2386033a35fabcd6d57c728eccb432800003751d
Brian <bwanjohi211@gmail.com>
2024-03-04 16:30:55 +0300

    displaying products

--- android/pages/ScanPage/ScanPage.tsx
+++ android/pages/ScanPage/ScanPage.tsx
@@ -1,17 +1,44 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState,useEffect} from 'react';
+import {StyleSheet, Text, View, Button, Image} from 'react-native';
 import {Camera, useCameraDevice, useCodeScanner} from 'react-native-vision-camera';
+import firestore  from '@react-native-firebase/firestore';
 
 const ScanPage = ({navigation}: {navigation:any}) => {
-      const device = useCameraDevice('front')
+      const device = useCameraDevice('back')
       const [barCode, setBarCode] = useState('')
+      const [scannedProduct, setScannedProduct] = useState('')
+      const [products, setProducts] = useState([])
 
       if (device == null) return <NoCameraDeviceError />
+
+      useEffect(() => {
+        async function fetchData() {
+          try {
+            const snapshot = await firestore().collection("Products").get();
+            const productList = snapshot.docs.map((doc) => doc.data());
+            setProducts(productList)
+            return productList;
+          } catch (error) {
+            console.error("Error fetching data:", error);
+            return null;
+          }
+        }
+
+        fetchData();
+      }, []);
+
 
 const codeScanner = useCodeScanner({
   codeTypes: ['qr', 'ean-13', 'ean-8'],
   onCodeScanned: (codes) => {
     console.log(codes[0].value)
+    let product = products.filter(product => product.code == codes[0].value)
+    if(product.length > 0){
+        setScannedProduct(product[0])
+        console.log(scannedProduct)
+    }else{
+        setScannedProduct('')
+    }
     setBarCode(codes[0].value)
   }
 })
@@ -24,8 +51,16 @@ const codeScanner = useCodeScanner({
               codeScanner={codeScanner}
               />
 
-            <Text>{barCode}</Text>
+              <View style={styles.container}>
+                <Image source={{uri: scannedProduct.imgUrl}}
+                    style={{width: 200, height: 200}} />
+                <Text style={styles.bigBlue}>{scannedProduct.name || 'Not found!'}</Text>
+                <Text style={styles.black}>Ksh: {scannedProduct.price || 'Not found!'}</Text>
               </View>
+
+
+
+        </View>
 
       )
     }
@@ -33,13 +68,23 @@ const codeScanner = useCodeScanner({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 5,
     padding: 20,
     backgroundColor: 'transparent',
   },
   camera: {
-    height: '29%',
+    height: '26%',
     margin: 20,
-  }
+  },
+  bigBlue: {
+      color: 'blue',
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+    black: {
+      color: 'black',
+      fontSize: 18,
+    }
 });
 
 export default ScanPage;
